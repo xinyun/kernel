7823c855ab39c586013afb9389cf867cf8dbe2aa
A6 <A6@qq.com>
2015-04-16 15:26:17 +0800

    删除hardware下没用目录

--- hardware/broadcom/wlan/bcmdhd/Android.mk
+++
@@ -1,3 +0,0 @@
-ifeq ($(BOARD_WLAN_DEVICE),bcmdhd_orig)
-    include $(call all-subdir-makefiles)
-endif
--- hardware/realtek/bluetooth/libbt-vendor/Android.mk
+++
@@ -1,3 +0,0 @@
-ifneq ($(BOARD_HAVE_BLUETOOTH_RTK),)
-    include $(call all-subdir-makefiles)
-endif
